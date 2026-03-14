c1eaeded4fa073cf36924560a64221c021980341
PawaOx4th <41177171+PawaOx4th@users.noreply.github.com>
2026-03-14 15:04:15 +0000

    chore(formula): update for v0.0.3

--- Formula/gsb.rb
+++ Formula/gsb.rb
@@ -1,11 +1,11 @@
 class Gsb < Formula
   desc "Pick a remote branch and switch to it quickly"
   homepage "https://github.com/PawaOx4th/gsb"
-  url "https://github.com/PawaOx4th/gsb/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "28c53bcb9517cd79293e69f139efcd61e15310cc906836f862077a29cd85e64a"
+  url "https://github.com/PawaOx4th/gsb/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "8103f53f746d7afc9a94c24d4e00e4240774b7d403e106e1b7561529a8f4caec"
   head "https://github.com/PawaOx4th/gsb.git", branch: "main"
   license "MIT"
-  version "0.0.2"
+  version "0.0.3"
 
   depends_on "git"
 
